b6e853540018744ffb85260897f83290652d8cba
alebrasi <alessandro.brasini1@gmail.com>
2022-11-16 16:56:30 +0100

    utils

---
+++ utils.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use('TKAgg')
+
+def find_file(relative_dir, file_name):
+    res = []
+    for path in Path(relative_dir).rglob(file_name):
+        res.append(path)
+    
+    if len(res) > 0:
+        return str(res[0])
+    
+    return ""
+
+def auto_canny(image, mask, sigma=0.33):
+	mask1 = mask == 255
+	# compute the median of the single channel pixel intensities
+	v = np.median(image[mask1])
+	# apply automatic Canny edge detection using the computed median
+	lower = int(max(0, (1.0 - sigma) * v))
+	upper = int(min(255, (1.0 + sigma) * v))
+	edged = cv.Canny(image, lower, upper)
+	# return the edged image
+	return edged
+
+def show_image(imgs, cmap='gray', cols=2):
+	n_imgs = len(imgs)
+	rows = (n_imgs // cols) + 1
+
+	if isinstance(imgs, tuple): 
+		img1, title = imgs
+		plt.title(title)
+		plt.imshow(img1, cmap=cmap)
+		plt.yticks([])
+		plt.xticks([])
+
+		plt.show()
+		return
+	
+	if isinstance(imgs, np.ndarray):
+		img1 = imgs
+		plt.yticks([])
+		plt.xticks([])
+		plt.imshow(imgs, cmap=cmap)
+
+		plt.show()
+		return
+
+	for i, img in enumerate(imgs):
+		img1 = None
+		title = ""
+
+		if isinstance(img, tuple) and len(img) == 2:
+			img1, title = img
+		else:
+			img1 = img
+
+		plt.subplot(rows, cols, i+1)
+		plt.title(title)
+		plt.yticks([])
+		plt.xticks([])
+		plt.imshow(img1, cmap=cmap)
+	
+	plt.subplots_adjust(bottom=0.0, right=0.679, left=0.364, top=0.871, wspace=0.0, hspace=0.052)
+	plt.show()
+		
+
+		
