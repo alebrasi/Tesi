7dd78e18801e0f24b8f1ac236d6c8e887b5f6c51
alebrasi <alessandro.brasini1@gmail.com>
2022-12-18 20:21:59 +0100

    Some advancements on root extraction

--- path_extraction/algorithm.py
+++ path_extraction/algorithm.py
@@ -2,6 +2,7 @@ import queue
 from enum import Enum
 from path_extraction.prune import valid_neighbors, seed_neighbors, root_neighbors, is_tip
 import numpy as np
+import math
 
 def lstsq_from_path(path):
     """
@@ -17,6 +18,53 @@ def lstsq_from_path(path):
     m, q = sol
 
     return m, q, residuals
+
+def angle_between_two_slopes(m1, m2):
+    return math.degrees(math.atan((m1 - m2) / 1 + (m1 * m2)))
+
+def points_to_vector(p1, p2):
+    """
+    Returns a normalized vector given two points
+
+    Parameters:
+        p1: Source point
+        p2: Destination point
+
+    Returns:
+        A numpy array containing the normalized vector
+    """
+
+    p1 = np.array(p1)
+    p2 = np.array(p2)
+
+    v = p1 - p2
+    return v / np.linalg.norm(v)
+
+def angle_between_vectors(v1, v2):
+    """
+    Returns the angle between two vectors normalized vectors
+    https://www.euclideanspace.com/maths/algebra/vectors/angleBetween/
+
+    Parameters:
+        v1: First normalized vector
+        v2: Second normalized vector
+    """
+
+    """
+    Alternativa: 
+    https://www.mathworks.com/matlabcentral/answers/180131-how-can-i-find-the-angle-between-two-vectors-including-directional-information
+    a = np.cross(v1, v2)
+    b = np.dot(v1, v2)
+    print(math.degrees(np.arctan2(a, b)))
+    """
+
+    v1_x, v1_y = v1[:2]
+    v2_x, v2_y = v2[:2]
+
+    angle = math.atan2(v2_y, v2_x) - math.atan2(v1_y, v1_x)
+    angle = math.degrees(angle)
+
+    return angle
 
 class RootsExtraction:
 
@@ -118,6 +166,33 @@ class RootsExtraction:
 
         return lines 
 
+    def __compare_paths(self, source_path, neighbors_paths):
+        """
+        Returns the
+        """
+        # Grab the last least squared path
+        mq, path = source_path[-1]
+        print(mq)
+
+        # For now the vector is created by taking the first and last node of the path
+        vs = points_to_vector(path[0], path[-1])
+
+        vn = []
+        min_angle_vector = (361, None)
+
+        # TODO: Try to make it with map() and min()
+        for n in neighbors_paths:
+            # Grab the first least squared path
+            _, path = n[0]
+            vn = points_to_vector(path[0], path[-1])
+            angle = abs(angle_between_vectors(vs, vn))
+            if angle < min_angle_vector[0]:
+                min_angle_vector = (angle, n)
+
+        smallest_angle_neighbor = min_angle_vector[1]
+
+        return smallest_angle_neighbor 
+        
 
     def extract_roots(self, s, max_residual_err=1.0, min_points_lstsq=4):
         """
@@ -145,11 +220,37 @@ class RootsExtraction:
 
         _, path = forking_node_paths[0]
         forking_node = path[-1]
+        
         neighbors = valid_neighbors(forking_node, root_neighbors, self.skel)
+        # Removes the father node
         neighbors.remove(path[-2])
+        """
+        root_paths = [path]
+
+        while True:
+            neighbors = valid_neighbors(forking_node, root_neighbors, self.skel)
+            # Removes the father node
+            neighbors.remove(path[-2])
+            tmp = []
+            for n in neighbors:
+                px_type, path = self.__walk_to_node(forking_node, n)
+                tmp.append(self.__extract_lstsq_from_path(path, max_residual_err, min_points_lstsq))
+            break
+        """
+
+        
+        #"""
         res = []
         for n in neighbors:
-            _, path = self.__walk_to_node(forking_node, n)
-            res.append(self.__extract_lstsq_from_path(path, max_residual_err, min_points_lstsq)) 
-
-        return res
+            _, path1 = self.__walk_to_node(forking_node, n)
+            print(path1)
+            a = self.__extract_lstsq_from_path(path1, max_residual_err, min_points_lstsq)
+            res.append(a)
+            
+        path = self.__extract_lstsq_from_path(path, max_residual_err, min_points_lstsq)
+        print("min: ", self.__compare_paths(path, res))
+        
+        stem_path = stem_paths[0]
+        _, stem_path = stem_path
+        return stem_path, res
+        #"""
