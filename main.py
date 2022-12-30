c0799f298d3fdcb09647ff819107c6296bb4320b
alebrasi <alessandro.brasini1@gmail.com>
2022-12-30 10:37:15 +0100

    Old main code. Ignore it

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2 as cv
 from skimage.morphology import skeletonize
 from utils import find_file, auto_canny, show_image, adjust_gamma, f
+import pydensecrf.densecrf as dcrf
+from pydensecrf.utils import unary_from_labels, create_pairwise_bilateral, create_pairwise_gaussian
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
@@ -34,6 +36,73 @@ def gain_division(img, ksize):
 
     return gainDivision
 
+def crf(anno_rgb, img, inference_steps):
+
+    # Convert the annotation's RGB color to a single 32-bit integer color 0xBBGGRR
+    anno_rgb = anno_rgb.astype(np.uint32)
+    anno_lbl = anno_rgb[:,:,0] + (anno_rgb[:,:,1] << 8) + (anno_rgb[:,:,2] << 16)
+
+
+    # Convert the 32bit integer color to 1, 2, ... labels.
+    # Note that all-black, i.e. the value 0 for background will stay 0.
+    colors, labels = np.unique(anno_lbl, return_inverse=True)
+
+    # But remove the all-0 black, that won't exist in the MAP!
+    HAS_UNK = 0 in colors
+    if HAS_UNK:
+        print("Found a full-black pixel in annotation image, assuming it means 'unknown' label, and will thus not be present in the output!")
+        print("If 0 is an actual label for you, consider writing your own code, or simply giving your labels only non-zero values.")
+        colors = colors[1:]
+    #else:
+    #    print("No single full-black pixel found in annotation image. Assuming there's no 'unknown' label!")
+
+    # And create a mapping back from the labels to 32bit integer colors.
+    colorize = np.empty((len(colors), 3), np.uint8)
+    colorize[:,0] = (colors & 0x0000FF)
+    colorize[:,1] = (colors & 0x00FF00) >> 8
+    colorize[:,2] = (colors & 0xFF0000) >> 16
+
+    # Compute the number of classes in the label image.
+    # We subtract one because the number shouldn't include the value 0 which stands
+    # for "unknown" or "unsure".
+    n_labels = len(set(labels.flat)) - int(HAS_UNK)
+    #print(n_labels, " labels", (" plus \"unknown\" 0: " if HAS_UNK else ""), set(labels.flat))
+
+    # Example using the DenseCRF2D code
+    d = dcrf.DenseCRF2D(img.shape[1], img.shape[0], n_labels)
+
+    # get unary potentials (neg log probability)
+    U = unary_from_labels(labels, n_labels, gt_prob=0.7, zero_unsure=HAS_UNK)
+    d.setUnaryEnergy(U)
+
+    # This adds the color-independent term, features are the locations only.
+    d.addPairwiseGaussian(sxy=(3, 3), compat=3, kernel=dcrf.DIAG_KERNEL,
+                          normalization=dcrf.NORMALIZE_SYMMETRIC)
+
+    # This adds the color-dependent term, i.e. features are (x,y,r,g,b).
+    d.addPairwiseBilateral(sxy=(80, 80), srgb=(13, 13, 13), rgbim=img,
+                           compat=10,
+                           kernel=dcrf.DIAG_KERNEL,
+                           normalization=dcrf.NORMALIZE_SYMMETRIC)
+
+    ####################################
+    ### Do inference and compute MAP ###
+    ####################################
+
+    # Run five inference steps.
+    Q = d.inference(inference_steps)
+
+    # Find out the most probable class for each pixel.
+    MAP = np.argmax(Q, axis=0)
+
+    # Convert the MAP (labels) back to the corresponding colors and save the image.
+    # Note that there is no "unknown" here anymore, no matter what we had at first.
+    MAP = colorize[MAP,:]
+    #imwrite(fn_output, MAP.reshape(img.shape))
+
+    return MAP.reshape(img.shape)
+
+
 def IASF(img, window_size):
     #gˆ(i, j) = a × gr(i, j) + b × g(i, j)
     #gr(i, j) represents the output of the improved range filter
@@ -51,11 +120,12 @@ mask_path = '/home/alebrasi/Documents/tesi/segmentate_prof/'
 mask_extension = 'bmp'
 image_extension = 'jpg'
 
-image_name = '109R'
+image_name = '950R'
 
 
 mask_path = find_file(mask_path, f'{image_name}.{mask_extension}')
 img_path = find_file(image_path, f'{image_name}.{image_extension}')
+
 
 print(f'Image path: {img_path}')
 print(f'Mask path: {mask_path}')
@@ -65,6 +135,14 @@ print(f'Mask path: {mask_path}')
 img = cv.imread(img_path)
 mask = cv.imread(mask_path, cv.COLOR_BGR2GRAY)
 
+h, w = mask.shape[0], mask.shape[1]
+
+mask_crf = np.zeros((h, w, 3))
+mask_crf[..., 1] = mask
+mask_crf[..., 2] = cv.bitwise_not(mask)
+
+
+
 ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
 mask = cv.morphologyEx(mask, cv.MORPH_DILATE, ker)
 
@@ -72,21 +150,14 @@ segmented = cv.bitwise_and(img, img, mask=mask)
 
 #segmented = segmented.astype(np.float32)
 
-vals = segmented[segmented > 0]
+segmented = cv.bilateralFilter(segmented, 9, 50, 50)
 
-print(np.median(vals))
-print(np.std(vals))
-print(np.max(vals))
-print(np.min(vals))
-
-print(np.percentile(vals, 95))
-print(np.percentile(vals, 5))
-
-segmented = cv.bilateralFilter(segmented, 9, 25, 25)
-
-f_img = f(segmented, alpha=3.0, beta=-450)
+f_img = f(segmented, alpha=3.0, beta=-400)
 
 g_img = adjust_gamma(f_img, 0.35)
+
+mask1 = crf(mask_crf, g_img, 300)
+show_image([mask, mask1])
 
 #g_img = cv.GaussianBlur(g_img, (3, 3), 10)
 
@@ -148,7 +219,7 @@ morph = cv.morphologyEx(labels, cv.MORPH_OPEN, ker)
 
 #blur = cv.bilateralFilter(l, 5, 30, 30) 
 #res = cv.ximgproc.anisotropicDiffusion(np.dstack((l, l, l)), 0.0075, 250, 1000)
-show_image([l, g_img[..., ::-1], labels, morph])
+show_image([l, g_img[..., ::-1], labels, mask1, morph])
 
 """
 while True:
