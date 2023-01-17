2cc2346cca763a0e6b740a1f3009be20d8f6b7de
alebrasi <alessandro.brasini1@gmail.com>
2023-01-17 16:34:23 +0100

    Code polishing

--- graph/plant.py
+++ graph/plant.py
@@ -12,11 +12,13 @@ class Plant:
 
     def __init__(self, seed, tip_start_node):
         self._seed = seed
-        self._roots = set()
+        #self._roots = set()
+        self._roots = []
         self._current_root = None
         self._tip_start_node = tip_start_node
         self._seed_node_neighbors = list(type(self).G.neighbors(seed))
         self._seed_node_neighbors.remove(tip_start_node)
+        type(self).G.edges[(seed, tip_start_node)]['walked'] = True
 
         self._init_new_root()
 
@@ -27,31 +29,28 @@ class Plant:
     def _init_new_root(self):
         new_root = None
         if len(self._seed_node_neighbors) == 0:
-            """
-            root = min(self._roots, 
-                        key=lambda r: r.highest_non_walked_node[2][0], 
-                        default=None)
-            """
-
+            # Finds the root that has the highest non walked neighbor
             root = min(
                         filter(lambda r: r[1] != None, 
-                            map(lambda r: (r, r.highest_non_walked_node), self._roots)), 
-                    key=lambda k: k[1][2][0],
-                    default=None)
+                                map(lambda r: (r, r.highest_non_walked_node), 
+                                    self._roots)
+                                ), 
+                        key=lambda k: k[1][2][0],
+                        default=None)
 
             if root == None:
                 return False
 
-            node = root[0].highest_non_walked_node
+            root, highest_node = root
+            node, neighbor, pos = highest_node
+
             print('plant: ', node)
             # FIXME: Fare la deepcopy degli edge della  root fino a quel nodo
-            #new_root = copy.deepcopy(root)
-            new_root = root[0].copy_until_node(node[0])
-            new_root._add_edge((node[0], node[1]))
+            new_root = root.copy_until_node(node, neighbor)
         else:
             new_root = self._init_new_root_starting_from_seed()
 
-        self._roots.add(new_root)
+        self._roots.append(new_root)
         self._current_root = new_root
 
         return True
