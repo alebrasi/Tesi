1d454362a8e1714ada97fb4a1f7a300837fad3a5
alebrasi <alessandro.brasini1@gmail.com>
2023-01-17 11:26:15 +0100

    Some bug fix

--- graph/root.py
+++ graph/root.py
@@ -1,21 +1,34 @@
+import copy
 import networkx as nx
+
+from graph.graph_creation import PointType
+
+flat_map = lambda f, xs: (y for ys in xs for y in f(ys))
 
 class Root:
 
-    flat_map = lambda f, xs: (y for ys in xs for y in f(ys))
     G = None
 
     @staticmethod
     def attach_graph(G):
         Root.G = G
 
-    def __init__(self, start_edge, ema_alpha=0.8):
+    def __init__(self, plant, start_edge, ema_alpha=0.8):
         self._edges = set()
         self._cur_edge = None
         self._prev_angle_ema = None
+        self._ordered_edges = []
         self._ema_alpha = ema_alpha
         self._start_edge = start_edge
+        self._plant = plant
         self._add_edge(start_edge)
+
+    def __str__(self):
+        return f'Start edge: {self._start_edge} \n \
+                Cur edge: {self._cur_edge} \n \
+                Seed: {self._plant._seed} \n \
+                Stem: {self._plant._tip_start_node} \n \
+                Edges: {self._ordered_edges} \n'
 
     def _add_edge(self, edge):
         """
@@ -24,7 +37,11 @@ class Root:
         Parameters:
             - edge (tuple) : tuple that describes the edge
         """
+        G = type(self).G
+        G.edges[edge]['walked'] = True
+        self._update_angle_EMA(edge)
         self._edges.add(edge)
+        self._ordered_edges.append(edge)
         self._cur_edge = edge
 
 #    def remove_edge(self, edge):
@@ -68,17 +85,64 @@ class Root:
     @property
     def highest_non_walked_node(self):
         G = type(self).G
+        """
+        non_walked_nodes = map(lambda n: (n, G.nodes[n]['pos']),
+                                filter(lambda n: n != self._plant._tip_start_node,
+                                    flat_map(lambda n: n, (self.non_walked_neighbors(node) 
+                                                        for node in self.nodes)
+                                    )
+                                )
+                            )
+        """
 
-        non_walked_nodes = map(lambda n: (n, G.nodes[n]['pos']), 
-                                (self.non_walked_neighbors(node) for node in self.nodes))
+        non_walked_nodes = []
 
-        return min(non_walked_nodes, 
-                    key=lambda n: n[1][0]  # key = y coordinate of 'pos'
-                   )[0]                    # Grabs only the node
+        for node in self.nodes:
+            neighbors = self.non_walked_neighbors(node)
+            for neighbor in neighbors:
+                if neighbor == self._plant._tip_start_node:
+                    continue
+                non_walked_nodes.append((node, neighbor, G.nodes[node]['pos']))
+
+        #non_walked_nodes = list(non_walked_nodes)
+        #print(id(self), " ", non_walked_nodes)
+        #print(non_walked_nodes)
+
+        return min(non_walked_nodes,
+                    key=lambda n: n[2][0],  # key = y coordinate of 'pos'
+                   default=None) #[0]                   # TODO: Sistemare.  Grabs only the node
+
+
+    def copy_until_node(self, node):
+            copied = copy.deepcopy(self)
+            tmp_edges = [ True if edge[1] == node else False for edge in copied._ordered_edges ]
+            #print(id(self))
+            print(node)
+            print(copied._ordered_edges)
+
+            idx = tmp_edges.index(True)
+            copied._cur_edge = copied._ordered_edges[idx]
+            copied._ordered_edges = copied._ordered_edges[:idx+1]
+            copied._edges.difference_update(tmp_edges[:idx+1])
+
+            # TODO: Scrivere MEGLIO
+            # calculate EMA angle
+            ema = type(self).G.edges[copied._ordered_edges[0]]['weight']
+            for edge in copied._ordered_edges:
+                angle = type(self).G.edges[edge]['weight']
+                ema = self._calc_ema(ema, angle, 0.8)
+
+            copied._prev_angle_ema = ema
+
+            return copied
 
     @property
     def nodes(self):
-        return set(flat_map(lambda e: e, self._edges))
+        tmp = set()
+        for e in self._edges:
+            tmp.add(e[1])
+        return tmp
+        #return set(flat_map(lambda e: e[1], self._edges))
 
     @property
     def edges(self):
@@ -95,8 +159,10 @@ class Root:
     def cur_edge(self):
         return self._cur_edge
 
-    @property
-    def _angle_EMA(self):
+    def _calc_ema(self, ema, angle, alpha):
+        return (alpha * angle) + ((1 - ema) * angle)
+
+    def _update_angle_EMA(self, edge):
         """
         # TODO Riscrivere meglio la doc
         Calculate the exponential moving average of the angle
@@ -106,20 +172,79 @@ class Root:
         Versione utilizzata:
         https://blog.mbedded.ninja/programming/signal-processing/digital-filters/exponential-moving-average-ema-filter/
         """
+        # TODO: Se funziona, rinominare self._prev_angle_ema in self._angle_ema
         
         G = type(self).G
-        cur_angle = G.edges[self._cur_edge]['weight']
+        #cur_angle = G.edges[self._cur_edge]['weight']
+        cur_angle = G.edges[edge]['weight']
 
         if self._prev_angle_ema == None:
             self._prev_angle_ema = cur_angle
             return cur_angle
 
+        """
         cur_angle_ema = (self._ema_alpha * cur_angle) +                 \
                         (1-self._ema_alpha) * self._prev_angle_ema
-
+        """
+        cur_angle_ema = self._calc_ema(self._prev_angle_ema, cur_angle, self._ema_alpha)
         self._prev_angle_ema = cur_angle_ema
-        return cur_angle_ema
+        
+        #return cur_angle_ema
+
+    def _compare_neighbor(self, node, neighbor):
+        G = type(self).G
+
+        edge = (node, neighbor)
+        return abs(self._prev_angle_ema - G.edges[edge]['weight'])
+
+    def _node_neighbors(self, edge):
+        """
+        Returns the neighbors of the destination node given the directed edge.
+        E.g. (0, 1) describes the edge from node 0 to 1:
+             the function returns the neighbors of 1 without the node 0
+
+        Parameters:
+            - node: the node
+            - prev_node: the predecessor node
+
+        Returns:
+        A list of neighbors excluded the predecessor node
+        """
+        G = type(self).G
+
+        prev_node, cur_node = edge
+        neighbors = list(G.neighbors(cur_node))
+        neighbors.remove(prev_node)
+
+        return neighbors
 
     def explore(self):
+        # Ti prego, scriviti da solo. Non ce la faccio più 😫
+
+        G = type(self).G
+
+        prev_node, cur_node = self._cur_edge
+
+        neighbors = self._node_neighbors(self._cur_edge)
+
+        # We reached a tip
+        if G.nodes[cur_node]['node_type'] == PointType.TIP:
+        #if len(neighbors) == 0:
+            return False
+
+        res = [ (n, self._compare_neighbor(cur_node, n)) for n in neighbors ]
+
+        # There's only one neighbor node: it can only go to that
+        if len(neighbors) == 1:
+            node, _ = res[-1]
+            self._add_edge((cur_node, node))
+
+            return True
+
+        # There are more neighbors
+
+        min_edge = min(res, key=lambda n: n[1])[0]
+        self._add_edge((cur_node, min_edge))
 
         return True
+
