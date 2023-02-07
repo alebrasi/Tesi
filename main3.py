6fcea2eac065c1ef22506f3428a394878a34577c
alebrasi <alessandro.brasini1@gmail.com>
2023-02-07 21:21:20 +0100

    Post segmentation rework

---
+++ main3.py
@@ -0,0 +1,130 @@
+import cv2 as cv
+from skimage.morphology import skeletonize, medial_axis, thin
+from skimage.graph import pixel_graph
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import math
+import argparse
+import sys
+
+from preprocess import adjust_gamma, automatic_brightness_and_contrast, clahe_bgr, remove_cc, locate_seed_line
+from utils import find_file, show_image, f, DebugContext
+from path_extraction.prune import prune_skeleton_branches, prune3
+from graph_test import extraction
+from refine_mask import refine_region_above, refine_region_below, locate_seeds
+
+def find_nearest(node, nodes):
+    node = np.array(node)
+    nodes = np.array(nodes)
+    d = np.linalg.norm(node-nodes, axis = 1)
+    p = np.argmin(d)
+
+    return nodes[p]
+
+matplotlib.use('TKAgg')
+
+#image_path = '/home/alebrasi/Documents/tesi/Dataset/sessioni'
+#mask_path = '/home/alebrasi/Documents/tesi/segmentate_prof/'
+
+image_path = '/home/alebrasi/Documents/tesi/Dataset/sessioni_crop/resize'
+mask_path = '/home/alebrasi/Documents/tesi/Dataset/sessioni_crop/segmentate_1024'
+
+mask_extension = 'png'
+image_extension = 'jpg'
+invert_mask = True
+
+# 88R, 89R SUS
+# Fare test su 498 e 87R
+# 105, 950R
+image_name = '109'
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('image_name', metavar='IMG')
+parser.add_argument('--no_extraction', action='store_true')
+parser.add_argument('--d_seed_line', action='store_true', default=False)
+parser.add_argument('--d_post_process', action='store_true', default=False)
+
+d_seed_line = False
+d_post_process = False
+no_extraction = False
+
+
+args = parser.parse_args()
+no_extraction = args.no_extraction
+image_name = args.image_name
+d_seed_line = args.d_seed_line
+d_post_process = args.d_post_process
+
+d_region_below = True
+d_region_above = True
+d_locate_seeds = True
+
+dbg_ctx_seed_line = DebugContext('seed_line', d_seed_line)
+dbg_ctx_post = DebugContext('post_process', d_post_process)
+dbg_ctx_region_below = DebugContext('region_below', d_region_below)
+dgb_ctx_region_above = DebugContext('region_abow', d_region_above)
+dbg_ctx_locate_seeds = DebugContext('locate_seeds', d_locate_seeds)
+
+mask_path = find_file(mask_path, f'{image_name}.{mask_extension}')
+img_path = find_file(image_path, f'{image_name}.{image_extension}')
+
+print(f'Image path: {img_path}')
+print(f'Mask path: {mask_path}')
+
+#img = cv.imread('/home/alebrasi/Documents/tesi/segmentate_prof/Sessione 1/Sessione 1.1/109.bmp')
+
+img = cv.imread(img_path)
+mask = cv.imread(mask_path, cv.COLOR_BGR2GRAY)
+
+if invert_mask:
+    mask = cv.bitwise_not(mask)
+    _, mask = cv.threshold(mask, 1, 255, cv.THRESH_BINARY)
+
+h, w = img.shape[:2]
+
+seed_line_roi = [[148, 0], [300, w-1]]
+
+M, left_pt, right_pt = locate_seed_line(img, seed_line_roi, 5, dbg_ctx=dbg_ctx_seed_line)
+
+# Apply rotation matrix to image and mask
+img = cv.warpAffine(img, M, (w, h))
+mask = cv.warpAffine(mask, M, (w, h))
+
+orig_img = img.copy()
+
+tmp = img.copy()
+cv.line(tmp, left_pt, right_pt, (0, 255, 0), 3)
+show_image(tmp[..., ::-1], dbg_ctx=dbg_ctx_seed_line)
+show_image(img[..., ::-1], dbg_ctx=dbg_ctx_seed_line)
+
+show_image(mask, dbg_ctx=dbg_ctx_seed_line)
+
+# ------------------- Mask refinement
+
+# Morphological closing for filling small gaps in the mask
+ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (15, 15))
+mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, ker)
+
+show_image(mask, dbg_ctx=dbg_ctx_post)
+
+segmented = cv.bitwise_and(img, img, mask=mask)
+
+show_image((segmented, "segm"), dbg_ctx=dbg_ctx_post)
+
+region_below = segmented[left_pt[1]:, :]
+region_above = segmented[:left_pt[1], :]
+
+arr = locate_seeds(region_above.copy(), dbg_ctx_locate_seeds)
+
+#h, w = segmented.shape[:2]
+#
+#segmented = np.zeros((h, w))
+#
+#segmented[left_pt[1]:, :] = refine_region_below(region_below)
+#segmented[:left_pt[1], :] = refine_region_above(region_above)
+##segmented[:left_pt[1], :] = mask[:left_pt[1], :]
+#
+#show_image(segmented)
+##show_image(f_img, dbg_ctx=dbg_ctx_post)
---
+++ refine_mask.py
@@ -0,0 +1,92 @@
+import cv2 as cv
+import numpy as np
+
+from preprocess import automatic_brightness_and_contrast, adjust_gamma
+from utils import show_image
+
+def refine_region_below(img, dbg_ctx):
+    f_img, _, _ = automatic_brightness_and_contrast(img, 40)
+    show_image((f_img, 'automatic brightness and contrast'), dbg_ctx=dbg_ctx)
+
+    blur = cv.pyrDown(f_img)
+    blur = cv.pyrUp(blur)
+    f_img = blur
+    #f_img = adjust_gamma(f_img, 2.0)
+    show_image((f_img, 'gamma corr'), dbg_ctx=dbg_ctx)
+
+    lab = cv.cvtColor(f_img, cv.COLOR_BGR2LAB)
+    l,a,b = cv.split(lab)
+    clahe = cv.createCLAHE(clipLimit=40.0, tileGridSize=(20, 20))
+    clahe_img = clahe.apply(l)
+
+    show_image([(l, 'luminosità'), (clahe_img, 'clahe')], dbg_ctx=dbg_ctx)
+
+    _, l = cv.threshold(clahe_img, 41, 255, cv.THRESH_TOZERO)
+    show_image(l)
+
+    thr = cv.adaptiveThreshold(
+        l, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 17, -2)
+
+    ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
+    morph = cv.morphologyEx(thr, cv.MORPH_CLOSE, ker)
+
+    show_image([(thr, 'thresholded'), (morph, 'closing')], dbg_ctx=dbg_ctx)
+
+    return morph
+
+def refine_region_above(img, dbg_ctx):
+    ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (21, 21))
+    morph = cv.morphologyEx(img, cv.MORPH_CLOSE, ker)
+    return morph
+
+def locate_seeds(img, dbg_ctx):
+    h, w = img.shape[:2]
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    show_image(hsv, cmap='magma', dbg_ctx=dbg_ctx)
+
+    #res = cv.inRange(hsv, (15, 80, 100), (35, 255, 255))
+    res = cv.inRange(hsv, (15, 100, 100), (25, 255, 255))
+    show_image([res, img[..., ::-1]], dbg_ctx=dbg_ctx)
+
+    ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7, 7))
+    morphed = cv.morphologyEx(res, cv.MORPH_CLOSE, ker)
+    # TODO: Dilate o close?
+    morphed = cv.morphologyEx(morphed, cv.MORPH_DILATE, ker)
+
+    _, labels, stats, _ = cv.connectedComponentsWithStats(morphed)
+
+    candidate_seeds_box = []
+    yy, xx = np.mgrid[:h, :w]
+
+    for i, stat in enumerate(stats[1:]):
+        i = i+1
+        if stat[cv.CC_STAT_AREA] < 200:
+            labels[i] = 0
+        else:
+            x, y = stat[cv.CC_STAT_LEFT], stat[cv.CC_STAT_TOP]
+            width, height = stat[cv.CC_STAT_WIDTH], stat[cv.CC_STAT_HEIGHT]
+            xs = xx[labels == i]
+            ys = yy[labels == i]
+            pos = np.column_stack((xs, ys))
+
+            rect = cv.minAreaRect(pos)
+            box = np.int0(cv.boxPoints(rect))
+            center, _, _ = rect
+            print(rect)
+            candidate_seeds_box.append((center, x, y, width, height))
+
+            print(f'Seed {i} BB: ')
+            print(f'\t x: {x}')
+            print(f'\t y: {y}')
+            print(f'\t w: {width}')
+            print(f'\t h: {height}')
+
+            cv.rectangle(img, (x, y), (x+width, y+height), (0, 255, 0), 1)
+            cv.drawContours(img, [box], 0, (0, 0, 255), 1)
+            cx, cy = np.int0(center)
+            img[cy, cx] = [255, 0, 0]
+
+    show_image(labels, cmap='magma', dbg_ctx=dbg_ctx)
+    show_image(img, dbg_ctx=dbg_ctx)
+
+    return candidate_seeds_box
