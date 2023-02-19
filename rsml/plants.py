24d406b3397297d9cfd699b033bb0451336ff3f0
alebrasi <alessandro.brasini1@gmail.com>
2023-02-19 21:05:13 +0100

    Added stem angle property

--- rsml/plants.py
+++ rsml/plants.py
@@ -1,11 +1,16 @@
 from .splines import Spline
 
 class Plant():
-    def __init__(self, id, label, seed=None, roots = None):
+    def __init__(self, id, label, stem, seed=None, roots = None):
         self.id = id
         self.label = label
+        self.stem = stem
         self.seed = seed
         self.roots = roots if roots is not None else []
+
+    @property
+    def stem_angle(self):
+        return self.stem.angle
 
     def all_roots(self):
         for r in self.roots:
@@ -27,10 +32,9 @@ class Plant():
                 yield c
 
 class Root():
-    def __init__(self, points, diameters, stem, roots = None, spline_tension = 0.5, spline_knot_spacing = 50):
+    def __init__(self, points, diameters, roots = None, spline_tension = 0.5, spline_knot_spacing = 50):
         self.roots = roots if roots is not None else []
         self.start = points[0]
         self.end = points[-1]
         self.diameters = diameters
-        self.stem = stem
         self.spline = Spline(points, spline_tension, spline_knot_spacing)
