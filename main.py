3aaeaa1a60d413ab4a595d206fb449e212db605a
alebrasi <alessandro.brasini1@gmail.com>
2023-03-03 18:39:57 +0100

    Removed old files

--- main.py
+++ main.py
@@ -1,131 +1,126 @@
 import cv2 as cv
-from skimage.morphology import skeletonize
-from utils import find_file, auto_canny, show_image, adjust_gamma, f
-import pydensecrf.densecrf as dcrf
-from pydensecrf.utils import unary_from_labels, create_pairwise_bilateral, create_pairwise_gaussian
+from skimage.morphology import skeletonize, medial_axis, thin
+from skimage.graph import pixel_graph
+from skimage.exposure import match_histograms
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib
+import matplotlib.pyplot as plt
+import math
+import argparse
+import sys
+import random
+import json
 
-def nothing(x):
-    pass
+from preprocess import adjust_gamma, automatic_brightness_and_contrast, clahe_bgr, remove_cc, locate_seed_line
+from utils import find_file, show_image, f, DebugContext
+from path_extraction.prune import prune_skeleton_branches, prune3
+from graph_test import extraction
+from refine_mask import refine_region_above, refine_region_below, locate_seeds
 
-def remove_small_cc(*args):
-    print(args)
-    
-def DoG(img, ksize, s1, s2):
-    low = cv.GaussianBlur(img, ksize, s1)
-    high = cv.GaussianBlur(img, ksize, s2)
+from graph.plant import Plant, Root
+from rsml.rsmlwriter import RSMLWriter
+from rsml.plants import Plant as RootNavPlant, Root as RootNavRoot
 
-    return low - high
+def find_nearest(node, nodes):
+    node = np.array(node)
+    nodes = np.array(nodes)
+    d = np.linalg.norm(node-nodes, axis = 1)
+    p = np.argmin(d)
 
-#https://stackoverflow.com/questions/65666507/local-contrast-enhancement-for-digit-recognition-with-cv2-pytesseract
-def gain_division(img, ksize):
-    # Get local maximum:
-    maxKernel = cv.getStructuringElement(cv.MORPH_RECT, (ksize, ksize))
-    localMax = cv.morphologyEx(img, cv.MORPH_CLOSE, maxKernel, None, None, 1, cv.BORDER_REFLECT101)
+    return nodes[p]
 
-    # Perform gain division
-    gainDivision = np.where(localMax == 0, 0, (img/localMax))
+def get_measures(plant_id, plant, distances):
+    # Longest root
 
-    # Clip the values to [0,255]
-    gainDivision = np.clip((255 * gainDivision), 0, 255)
+    measures = {}
 
-    # Convert the mat type from float to uint8:
-    gainDivision = gainDivision.astype("uint8")
+    measures['plant_id'] = plant_id
+    roots = plant.roots
+    longest_root = max(roots, key=lambda r: len(r.points))
+    print('Max root lenght: ', len(root.points))
+    measures['max_root_lenght'] = len(root.points)
 
-    return gainDivision
+    # Num roots
+    print('Number of roots:', len(plant.roots))
+    measures['roots_number'] = len(plant.roots)
 
-def crf(anno_rgb, img, inference_steps):
+    # Stem angle
+    print('Stem angle: ', plant.stem.angle)
+    measures['stem_angle'] = round(plant.stem.angle, 2)
 
-    # Convert the annotation's RGB color to a single 32-bit integer color 0xBBGGRR
-    anno_rgb = anno_rgb.astype(np.uint32)
-    anno_lbl = anno_rgb[:,:,0] + (anno_rgb[:,:,1] << 8) + (anno_rgb[:,:,2] << 16)
+    # Root thickness
+    roots_thickness = []
+    all_points = set()
+    measures['roots_thickness'] = {}
+    for i, r in enumerate(roots):
+        points = r.points
+        tmp = list(map(lambda p: distances[p], points))
+        mean_thickness = sum(tmp)/len(tmp)
+        measures['roots_thickness'][i+1] = round(mean_thickness, 2)
 
+        print(f'Root {i+1}  mean thickness: ', mean_thickness)
+        for i, point in enumerate(points):
+            roots_thickness.append(tmp[i])
+            y, x = point
+            all_points.add((x, y))
 
-    # Convert the 32bit integer color to 1, 2, ... labels.
-    # Note that all-black, i.e. the value 0 for background will stay 0.
-    colors, labels = np.unique(anno_lbl, return_inverse=True)
+    avg_roots_thickness = sum(roots_thickness)/len(roots_thickness)
+    print('Average roots thickness: ', avg_roots_thickness)
+    measures['average_roots_thickness'] = round(avg_roots_thickness, 2)
 
-    # But remove the all-0 black, that won't exist in the MAP!
-    HAS_UNK = 0 in colors
-    if HAS_UNK:
-        print("Found a full-black pixel in annotation image, assuming it means 'unknown' label, and will thus not be present in the output!")
-        print("If 0 is an actual label for you, consider writing your own code, or simply giving your labels only non-zero values.")
-        colors = colors[1:]
-    #else:
-    #    print("No single full-black pixel found in annotation image. Assuming there's no 'unknown' label!")
+    # Convex hull
+    hull = cv.convexHull(np.array(list(all_points)))
+    hull_area = cv.contourArea(hull)
+    print('Plant convex hull: ', hull_area)
+    measures['convex_hull'] = round(hull_area, 2)
 
-    # And create a mapping back from the labels to 32bit integer colors.
-    colorize = np.empty((len(colors), 3), np.uint8)
-    colorize[:,0] = (colors & 0x0000FF)
-    colorize[:,1] = (colors & 0x00FF00) >> 8
-    colorize[:,2] = (colors & 0xFF0000) >> 16
-
-    # Compute the number of classes in the label image.
-    # We subtract one because the number shouldn't include the value 0 which stands
-    # for "unknown" or "unsure".
-    n_labels = len(set(labels.flat)) - int(HAS_UNK)
-    #print(n_labels, " labels", (" plus \"unknown\" 0: " if HAS_UNK else ""), set(labels.flat))
-
-    # Example using the DenseCRF2D code
-    d = dcrf.DenseCRF2D(img.shape[1], img.shape[0], n_labels)
-
-    # get unary potentials (neg log probability)
-    U = unary_from_labels(labels, n_labels, gt_prob=0.7, zero_unsure=HAS_UNK)
-    d.setUnaryEnergy(U)
-
-    # This adds the color-independent term, features are the locations only.
-    d.addPairwiseGaussian(sxy=(3, 3), compat=3, kernel=dcrf.DIAG_KERNEL,
-                          normalization=dcrf.NORMALIZE_SYMMETRIC)
-
-    # This adds the color-dependent term, i.e. features are (x,y,r,g,b).
-    d.addPairwiseBilateral(sxy=(80, 80), srgb=(13, 13, 13), rgbim=img,
-                           compat=10,
-                           kernel=dcrf.DIAG_KERNEL,
-                           normalization=dcrf.NORMALIZE_SYMMETRIC)
-
-    ####################################
-    ### Do inference and compute MAP ###
-    ####################################
-
-    # Run five inference steps.
-    Q = d.inference(inference_steps)
-
-    # Find out the most probable class for each pixel.
-    MAP = np.argmax(Q, axis=0)
-
-    # Convert the MAP (labels) back to the corresponding colors and save the image.
-    # Note that there is no "unknown" here anymore, no matter what we had at first.
-    MAP = colorize[MAP,:]
-    #imwrite(fn_output, MAP.reshape(img.shape))
-
-    return MAP.reshape(img.shape)
-
-
-def IASF(img, window_size):
-    #gˆ(i, j) = a × gr(i, j) + b × g(i, j)
-    #gr(i, j) represents the output of the improved range filter
-    #a and b are the normalized weighted factors of the improved range filter 
-    #and the average filter respectively
-
-    return
+    return measures
 
 
 matplotlib.use('TKAgg')
+cv.setRNGSeed(123)
+random.seed(123)
 
-image_path = '/home/alebrasi/Documents/tesi/Dataset/sessioni'
-mask_path = '/home/alebrasi/Documents/tesi/segmentate_prof/'
+image_path = '/home/alebrasi/Documents/tesi/Dataset/sessioni_crop/resize'
+mask_path = '/home/alebrasi/Documents/tesi/Dataset/sessioni_crop/segmentate_1024'
 
-mask_extension = 'bmp'
+mask_extension = 'png'
 image_extension = 'jpg'
+invert_mask = True
 
-image_name = '950R'
+image_name = '1004'
+reference_hist_img_name = '995'
 
+parser = argparse.ArgumentParser()
+parser.add_argument('image_name', metavar='IMG')
+parser.add_argument('--no_extraction', action='store_true')
+parser.add_argument('--d_seed_line', action='store_true', default=False)
+parser.add_argument('--d_post_process', action='store_true', default=False)
+
+d_seed_line = False
+d_post_process = False
+no_extraction = False
+d_region_below = False
+d_region_above = False
+d_locate_seeds = False
+
+#"""
+args = parser.parse_args()
+no_extraction = args.no_extraction
+image_name = args.image_name
+d_seed_line = args.d_seed_line
+d_post_process = args.d_post_process
+#"""
+
+dbg_ctx_seed_line = DebugContext('seed_line', d_seed_line)
+dbg_ctx_post = DebugContext('post_process', d_post_process)
+dbg_ctx_region_below = DebugContext('region_below', d_region_below)
+dgb_ctx_region_above = DebugContext('region_abow', d_region_above)
+dbg_ctx_locate_seeds = DebugContext('locate_seeds', d_locate_seeds)
 
 mask_path = find_file(mask_path, f'{image_name}.{mask_extension}')
 img_path = find_file(image_path, f'{image_name}.{image_extension}')
-
+reference_hist_path = find_file(image_path, f'{reference_hist_img_name}.{image_extension}')
 
 print(f'Image path: {img_path}')
 print(f'Mask path: {mask_path}')
@@ -134,117 +129,173 @@ print(f'Mask path: {mask_path}')
 
 img = cv.imread(img_path)
 mask = cv.imread(mask_path, cv.COLOR_BGR2GRAY)
-
-h, w = mask.shape[0], mask.shape[1]
-
-mask_crf = np.zeros((h, w, 3))
-mask_crf[..., 1] = mask
-mask_crf[..., 2] = cv.bitwise_not(mask)
+reference_hist_image = cv.imread(reference_hist_path)
 
 
+show_image(mask)
+if invert_mask:
+    mask = cv.bitwise_not(mask)
+    _, mask = cv.threshold(mask, 1, 255, cv.THRESH_BINARY)
+    show_image(mask, dbg_ctx=dbg_ctx_post)
 
-ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
-mask = cv.morphologyEx(mask, cv.MORPH_DILATE, ker)
+h, w = img.shape[:2]
 
-segmented = cv.bitwise_and(img, img, mask=mask)
+seed_line_roi = [[148, 0], [300, w-1]]
 
-#segmented = segmented.astype(np.float32)
+M, left_pt, right_pt = locate_seed_line(img, seed_line_roi, 5, dbg_ctx=dbg_ctx_seed_line)
 
-segmented = cv.bilateralFilter(segmented, 9, 50, 50)
+# Alignment in order to subdivide the above and below regions with even height
+# Must be done for image pyramids upscale
+if ((h - left_pt[1]) % 2) == 1:
+    y = left_pt[1] + 1
+    x1 = left_pt[0]
+    x2 = right_pt[0]
 
-f_img = f(segmented, alpha=3.0, beta=-400)
+    left_pt = (x1, y)
+    right_pt = (x2, y)
 
-g_img = adjust_gamma(f_img, 0.35)
+# Apply rotation matrix to image and mask
+img = cv.warpAffine(img, M, (w, h))
+mask = cv.warpAffine(mask, M, (w, h))
 
-mask1 = crf(mask_crf, g_img, 300)
-show_image([mask, mask1])
+orig_img = img.copy()
 
-#g_img = cv.GaussianBlur(g_img, (3, 3), 10)
+tmp = img.copy()
+cv.line(tmp, left_pt, right_pt, (0, 255, 0), 3)
+show_image(tmp[..., ::-1], dbg_ctx=dbg_ctx_seed_line)
+show_image(img[..., ::-1], dbg_ctx=dbg_ctx_seed_line)
 
-ker = np.array([[0, -1, 0],
-                [-1, 5,-1],
-                [0, -1, 0]])
+show_image(mask, dbg_ctx=dbg_ctx_seed_line)
 
-#g_img = cv.filter2D(g_img, -1, ker)
+# ------------------- Mask preparation ------------------------------
 
-#hls = cv.cvtColor(g_img, cv.COLOR_BGR2HLS)
-#h,l,s = cv.split(hls)
+# Morphological closing for filling small gaps in the mask
+ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (15, 15))
+mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, ker)
 
-l1 = cv.cvtColor(g_img, cv.COLOR_BGR2GRAY)
+show_image((mask, 'maschera'), dbg_ctx=dbg_ctx_post)
 
-clahe = cv.createCLAHE(clipLimit=2, tileGridSize=(30, 30))
-l = clahe.apply(l1)
+matched_hist_img = match_histograms(img, reference_hist_image, channel_axis=-1)
+show_image([img[..., ::-1], matched_hist_img[..., ::-1]], dbg_ctx=dbg_ctx_post)
+segmented = cv.bitwise_and(matched_hist_img, matched_hist_img, mask=mask)
+show_image((segmented[..., ::-1], 'segmentata'), dbg_ctx=dbg_ctx_post)
 
-show_image([l1, l])
-#l = gain_division(l, 12)
+region_below = segmented[left_pt[1]:, :]
+region_above = segmented[:left_pt[1], :]
+mask_above = mask[:left_pt[1], :]
+mask_below = mask[left_pt[1]:, :]
+show_image(region_below[..., ::-1])
+# ------------ Seeds localization ----------------------------------
+top_left_pt, bottom_right_pt = seed_line_roi[0], seed_line_roi[1]
+br_y, br_x = bottom_right_pt
+seeds_roi = img[:br_y, :br_x]
 
-#show_image([l, g_img[..., ::-1], thr])
+seeds_bb = locate_seeds(seeds_roi.copy(), dbg_ctx_locate_seeds)
+# -----------------------------------------------------------------
 
-#canny = auto_canny(s, mask)
+# -------------- Mask refinement ------------------
+h, w = segmented.shape[:2]
+refined_mask = np.zeros((h, w), dtype=np.uint8)
 
-#dxdy = cv.Sobel(s, cv.CV_32F, 0, 1)
+refined_mask[left_pt[1]:, :] = refine_region_below(region_below, mask_below, dbg_ctx=dbg_ctx_region_below)
+refined_mask[:left_pt[1], :] = refine_region_above(mask_above, dgb_ctx_region_above)
 
-#show_image([segmented[..., ::-1], s, dxdy])
+# Morphological closing inside seeds bounding box
+ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (11, 11))
+for bb in seeds_bb:
+    x, y, w, h = bb
+    tmp = refined_mask[y:y+h, x:x+w]
+    refined_mask[y:y+h, x:x+w] = cv.morphologyEx(tmp, cv.MORPH_CLOSE, ker)
+show_image(refined_mask, dbg_ctx=dbg_ctx_post)
+smoothed_mask = cv.medianBlur(refined_mask, 5)       # Edge smoothing
 
+#ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3,3))
+#asd = cv.morphologyEx(asd, cv.MORPH_ERODE, ker)
 
-"""
-cv.namedWindow('Window')
+# Filling gaps with area < 30 px
+cnts, _ = cv.findContours(smoothed_mask, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+for i, cnt in enumerate(cnts):
+    if cv.contourArea(cnt) < 30:
+        cv.drawContours(smoothed_mask, cnts, i, 255, -1)
 
-cv.createTrackbar('Sigma1', 'Window', 0, 500, nothing)
-cv.createTrackbar('Sigma2', 'Window', 0, 500, nothing)
-#cv.createTrackbar('Ksize', 'Window', 3, 21, nothing)
-"""
+show_image([(refined_mask, 'maschera rifinita'), (smoothed_mask, 'bordi smussati')], dbg_ctx=dbg_ctx_post)
+show_image((cv.bitwise_and(orig_img, orig_img, mask=smoothed_mask)[..., ::-1], 'risultato segmentazione'), dbg_ctx=dbg_ctx_post)
+show_image((smoothed_mask, "bordi smussati"), dbg_ctx=dbg_ctx_post)
+refined_mask = smoothed_mask
+# -------------------------------------------------------------------------------
 
-#clahe = cv.createCLAHE(clipLimit=0.1)
-#l = clahe.apply(l) 
+# ----------------- Skeletonization and prune -----------------------------------
 
+skeleton, dist = medial_axis(refined_mask.astype(bool), return_distance=True, random_state=123)
+show_image(skeleton, dbg_ctx=dbg_ctx_post)
+skeleton = skeleton.astype(np.uint8)
+cross_ker = np.array([[-1, 1, -1],
+                      [1, -1, 1],
+                      [-1, 1, -1]], dtype=np.uint8)
+cross_nodes = cv.morphologyEx(skeleton, cv.MORPH_HITMISS, cross_ker)
+skeleton = skeleton | cross_nodes
 
-#_, thr = cv.threshold(l, 0, 255, cv.THRESH_OTSU)
+pruned_skeleton = prune3(skeleton, 10)
+pruned_skeleton = thin(pruned_skeleton.astype(bool)).astype(np.uint8)
 
-thr = cv.adaptiveThreshold(l, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 5, 0)
+show_image([(skeleton, 'scheletro'), (pruned_skeleton, 'scheletro con branch <= 10px potati')], dbg_ctx=dbg_ctx_post)
 
-_, labels, stats, _ = cv.connectedComponentsWithStatsWithAlgorithm(thr, 4, cv.CV_16U, cv.CCL_DEFAULT)
+pruned_skeleton = pruned_skeleton.astype(bool)
+# -------------------------------------------------------------------------------
 
-for label, stat in enumerate(stats):
-    if stat[cv.CC_STAT_AREA] < 25:
-        labels[labels == label] = 0
+tmp = pruned_skeleton.copy().astype(np.uint8)
+tmp = cv.cvtColor(tmp, cv.COLOR_GRAY2BGR)
+tmp[pruned_skeleton, ...] = [255, 255, 255]
+seeds_pos = []
+for bb in seeds_bb:
+    x, y, w, h = bb
 
-labels[labels > 0] = 255
+    centroid = (y+h//2, x+w//2)
+    arr = pruned_skeleton[y:y+h, x:x+w]
+    nodes = np.argwhere(arr) + [y, x]
+    if len(nodes) > 0:
+        nearest = find_nearest(centroid, nodes)
+        cv.rectangle(tmp, bb, (0,255,0), 1)
+        a, b = nearest
+        tmp[centroid[0], centroid[1], ...] = [0,255,255]
+        tmp[a, b, ...] = [0, 0,255]
+        seeds_pos.append((a, b))
+show_image(tmp[:, ::-1, ::-1], dbg_ctx=dbg_ctx_post)
+print(seeds_pos)
+show_image(pruned_skeleton, dbg_ctx=dbg_ctx_post)
 
-labels = labels.astype(np.uint8)
+plants = extraction(seeds_pos, pruned_skeleton, dist, orig_img)
 
-ker = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
+spline_parameters = {
+                        'tension': 0.5,
+                        'spacing': 50 
+                    }
 
-morph = cv.morphologyEx(labels, cv.MORPH_OPEN, ker)
+# Create Plant structure
+rootnav_plants = [ RootNavPlant(idx, 'orzo', p.stem, p.seed_coords) for idx, p in enumerate(plants) ]
 
-#blur = cv.bilateralFilter(l, 5, 30, 30) 
-#res = cv.ximgproc.anisotropicDiffusion(np.dstack((l, l, l)), 0.0075, 250, 1000)
-show_image([l, g_img[..., ::-1], labels, mask1, morph])
+measures = {}
+measures['plants'] = []
 
-"""
-while True:
-    cv.imshow('Window', dog)
+for i, plant in enumerate(plants):
+    for root in plant.roots:
+        points = root.points
+        diameters = [ dist[p] for p in points ]
+        points = [ (x, y) for y, x in points ]
+        tmp_root = RootNavRoot(points, 
+                                diameters, 
+                                spline_tension=spline_parameters['tension'], 
+                                spline_knot_spacing=spline_parameters['spacing']
+                              )
+        rootnav_plants[i].roots.append(tmp_root)
+    measures['plants'].append(get_measures(i+1, plant, dist))
 
-    s1 = cv.getTrackbarPos('Sigma1', 'Window')
-    s2 = cv.getTrackbarPos('Sigma2', 'Window')
-    #ksize = cv.getTrackbarPos('Ksize', 'Window')
+output_dir = '/home/alebrasi/Videos/rsml_tests'
 
-    dog = DoG(l, ksize, s1, s2)
+# Dumps measures to json
+with open(f'{output_dir}/{image_name}.json', 'w', encoding='utf-8') as f:
+    json_measures = json.dumps(measures, ensure_ascii=False, indent=4)
+    f.write(json_measures)
 
-    canny = cv.Canny(s, thr1, thr2)
-
-    _, labels, stats, _ = cv.connectedComponentsWithStats(canny)
-
-    for label, stat in enumerate(stats):
-        if stat[cv.CC_STAT_AREA] < 25:
-            labels[labels == label] = 0
-
-    labels[labels > 0] = 255
-
-
-    k = cv.waitKey(20)
-    if k == 27:
-        break
-
-cv.destroyAllWindows()
-"""
+# Output to RSML
+RSMLWriter.save(image_name, output_dir, rootnav_plants)
